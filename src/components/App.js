d609a7e3efae68b6e7a25c3183f8bc5d30207b32
Zahnen Garner <zahnen@gmail.com>
2021-03-17 11:43:07 -0700

    add Header and Body component, parallax scrolling

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+import Body from './Body';
+import '../App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  );
+}
+
+export default App;
