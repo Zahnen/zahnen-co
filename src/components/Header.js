e2eea95bb7b3938d5cf7e51335d9bcf226b05bb5
Zahnen Garner <zahnen@gmail.com>
2021-03-18 12:38:02 -0700

    add profile photo, initial styling for header, bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import ProfilePicture from '../img/zahnen2.jpg';
 
 const divStyle = {
 	backgroundColor: 'darkgray',
-	width: '30%',
+	width: '40%',
 	height: '100vh'
+};
+
+const div2Style = {
+  marginTop: '35%'
 };
 
 function Header() {
   return (
     <>
       <div style={divStyle}>
-        <h1>Zahnen Garner</h1>
-        <p>Github: @Zahnen</p>
-        <p>LinkedIn: @Zahnen</p>
+        <div style={div2Style}>
+          <img src={ProfilePicture} alt="Zahnen" width="25%" style={{borderRadius: "50%"}} className="img-fluid"></img>
+          <h1>I'm Zahnen, and I'm a baby developer.</h1>
+          <h3>I enjoy working collaboratively to bring ideas to life in digital spaces.</h3>
+          <button className="btn btn-outline-dark rounded-pill">Let's Connect</button>
+        </div>
+        <div>
+          <p>Github: @Zahnen</p>
+          <p>LinkedIn: @Zahnen</p>
+        </div>
       </div>
     </>
   );
