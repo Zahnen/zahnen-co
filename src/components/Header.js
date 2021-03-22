2cc7c180bd6603b0a5e3c5df35246ab186160249
Zahnen Garner <zahnen@gmail.com>
2021-03-22 14:23:11 -0700

    move GitHub/LinkedIn links to Links component, add Say Hi svg

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ProfilePicture from '../img/zahnen2.jpg';
-import LinkedIn from '../img/linkedinmark.svg';
-import GitHub from '../img/githubmark.svg';
+import Connect from '../img/connect.svg';
 
 const divStyle = {
 	backgroundColor: 'white',
@@ -20,11 +19,9 @@ function Header() {
         <div style={div2Style}>
           <img src={ProfilePicture} alt="Zahnen" width="35%" className="rounded mx-auto d-block"></img>
           <h1>I'm Zahnen, and I'm a baby developer.</h1>
-          <button className="btn btn-outline-dark rounded-pill">Let's Connect</button>
         </div>
         <div>
-          <img src={GitHub} alt="GitHub Logo" href="https://github.com/Zahnen" />
-          <img src={LinkedIn} alt="LinkedIn Logo" href="https://www.linkedin.com/in/zahnen/" />
+          <a href="mailto:hello@zahnen.co" target="_blank" rel="noreferrer"><img src={Connect} alt="Say Hi" /></a>
         </div>
       </div>
     </>
