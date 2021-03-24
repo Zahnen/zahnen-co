b348df28d64891910f4b578441f69b5ebd7a535e
Zahnen Garner <zahnen@gmail.com>
2021-03-23 17:10:11 -0700

    add Personal component, update Personal copy

---
+++ src/components/Personal.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Personal() {
+  return (
+    <>
+      <div style={{marginTop: '15%', width: "60%"}}>
+        <h1><b>Personal</b></h1>
+        <hr />
+        <h3>If I'm not coding, you'll probably find me baking, playing video games that frustrate me, watching sub-par television, collecting tiny things, etc.</h3>
+      </div>
+    </>
+  );
+}
+
+export default Personal;
