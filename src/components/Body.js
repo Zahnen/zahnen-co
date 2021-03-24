06cc7f33f96681a245c9c3ebcddec4f0e7eca47a
Zahnen Garner <zahnen@gmail.com>
2021-03-23 17:12:37 -0700

    restructure Body component with updated components

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,33 +1,36 @@
 import React from 'react';
 import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons';
 import Projects from './Projects';
+import Tech from './Tech';
+import About from './About';
+import Personal from './Personal';
+import Arrow from './Arrow';
+
 
 class Body extends React.Component {
   render(){
     return (
       <>
         <div>
-          <Parallax ref={ref => (this.parallax = ref)} pages={3}>
-            <ParallaxLayer offset={0} speed={0.1}>
+          <Parallax ref={ref => (this.parallax = ref)} pages={2.5}>
+            <ParallaxLayer offset={0} speed={0}>
+              <About />
+            </ParallaxLayer>
+            <ParallaxLayer offset={.7} speed={-0.1}>
+              <Arrow />
+            </ParallaxLayer>
+            <ParallaxLayer offset={.8} speed={.6}>
               <div style={{marginTop: '15%', width: "60%"}}>
-                <h1>About</h1>
-                <hr />
-                <h3>I'm a web developer based in Portland, Oregon. I enjoy working collaboratively to bring ideas to life in digital spaces.</h3>
-                <h1>Tech</h1>
-                <hr />
-                <h3>I have experience with ReactJS, C#, JavaScript, Node.js, ASP.NET, MySQL, NoSQL, REST APIs, Git/GitHub</h3>
+                <Tech />
               </div>
             </ParallaxLayer>
-            <ParallaxLayer offset={.8} speed={.4}  style={{backgroundColor: 'white' }}>
-              <div style={{marginTop: '15%', width: "60%"}}>
-                <h1>PlaceHolder</h1>
-                <hr />
-              </div>
-            </ParallaxLayer>
-            <ParallaxLayer offset={1.6} speed={.6} style={{backgroundColor: 'white' }}>
+            <ParallaxLayer offset={1} speed={.4}>
               <div style={{marginTop: '15%', width: "60%"}}>
                 <Projects />
               </div>
+            </ParallaxLayer>
+            <ParallaxLayer offset={1.8} speed={.6}>
+              <Personal />
             </ParallaxLayer>
           </Parallax>
         </div>
