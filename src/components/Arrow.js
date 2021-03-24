e314b0b8c3c77deadff2bfefb12970a920876df2
Zahnen Garner <zahnen@gmail.com>
2021-03-23 17:09:45 -0700

    add Arrow component

---
+++ src/components/Arrow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavArrow from '../img/navarrow.svg';
+
+function Arrow() {
+  return (
+    <>
+      <img src={NavArrow} alt="Navigation Arrow Pointing Down" style={{marginRight: '1%'}}/>
+    </>
+  );
+}
+
+export default Arrow;
