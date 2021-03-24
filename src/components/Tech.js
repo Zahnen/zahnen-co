7949404e4141dfa73c51c3b6a676705b5c22e79b
Zahnen Garner <zahnen@gmail.com>
2021-03-23 17:11:43 -0700

    add Tech component, update Tech copy

---
+++ src/components/Tech.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Tech() {
+  return (
+    <>
+      <div style={{marginTop: '10%', float: 'right', width: "60%"}}>
+        <h1><b>Tech</b></h1>
+        <hr />
+        <h3>Languages: JavaScript, C#, HTML, CSS</h3>
+        <h3>Libraries: ReactJS, Redux, Bootstrap, react-spring</h3>
+        <h3>Frameworks: ASP.NET, Express</h3>
+        <h3>Databases: MySQL, Firebase</h3>
+        <h3>Workflow: Git/GitHub, AGILE/Scrum, Discord, Slack</h3>
+      </div>
+    </>
+  );
+}
+
+export default Tech;
